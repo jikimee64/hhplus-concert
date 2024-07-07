da23cbaee1eede731bbfe8fd615ee58932593e85
jikimee64 <jikimee64@gmail.com>
2024-07-08 00:05:57 +0900

    test: @SpringBootTest 추상화

---
+++ src/test/java/hhplus/concert/IntegrationTest.java
@@ -0,0 +1,8 @@
+package hhplus.concert;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IntegrationTest {
+
+}
--- src/test/java/hhplus/concert/domain/UserQueueManagerTest.java
+++ src/test/java/hhplus/concert/domain/UserQueueManagerTest.java
@@ -1,17 +1,16 @@
 package hhplus.concert.domain;
 
+import hhplus.concert.IntegrationTest;
 import hhplus.concert.infra.jwt.JwtQueueTokenProviderTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SpringBootTest
-class UserQueueManagerTest {
+class UserQueueManagerTest extends IntegrationTest {
 
     @Autowired
     private UserQueueRepository userQueueRepository;
@@ -34,6 +33,22 @@ class UserQueueManagerTest {
                 () -> assertThat(testQueueToken).isEqualTo(queueToken),
                 () -> assertThat(userQueues).hasSize(1)
         );
+    }
+
+    @Test
+    void 제한인원이_50명인_큐에서_PROGRESS_상태인_마지막_대기열_번호가_50번이고_유저의_대기열_번호가_51일_경우_대기순번_1을_반환한다() {
+        // given
+        Long userId = 1L;
+        Long userWaitingNumber = 51L;
+        QueueTokenProvider queueTokenProvider = new JwtQueueTokenProviderTest(userId, userWaitingNumber);
+        UserQueueManager userQueueManager = new UserQueueManager(userQueueRepository, queueTokenProvider);
+        Long concertScheduleId = 1L;
+
+        // when
+        Long waitingNumber = userQueueManager.selectWaitingNumber(concertScheduleId, userId, userWaitingNumber);
+
+        // then
+        assertThat(waitingNumber).isEqualTo(1L);
     }
 
 }
