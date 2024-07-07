927d6eb5aa1f9d195b56305e27239da6b6759cec
jikimee64 <jikimee64@gmail.com>
2024-07-07 16:05:08 +0900

    chore: 유저 API swagger 설정 추가

--- src/main/java/hhplus/concert/api/v1/user/reqeust/ChargeCashRequest.java
+++ src/main/java/hhplus/concert/api/v1/user/reqeust/ChargeCashRequest.java
@@ -1,6 +1,9 @@
 package hhplus.concert.api.v1.user.reqeust;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ChargeCashRequest(
+        @Schema(description = "충전 잔액")
         Integer amount
 ) {
 }
