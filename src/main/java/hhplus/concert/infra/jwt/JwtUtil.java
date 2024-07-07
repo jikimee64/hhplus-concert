ad003fa2dad9c2b6cb1a2a77fe90b9b3597f5755
jikimee64 <jikimee64@gmail.com>
2024-07-07 20:10:32 +0900

    feat: jwt 토큰 기능 추가

---
+++ src/main/java/hhplus/concert/infra/jwt/JwtUtil.java
@@ -0,0 +1,63 @@
+package hhplus.concert.infra.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final Long expiredMs;
+
+    public JwtUtil(
+            @Value("${spring.jwt.secret}") String secret,
+            @Value("${spring.jwt.expiredMs}") Long expiredMs
+    ) {
+        secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+        this.expiredMs = expiredMs;
+    }
+
+    public Long getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("userId", Long.class);
+    }
+
+    public Long getWaitingNumber(String token) {
+        return Jwts.parser().verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("waitingNumber", Long.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String createJwt(Long userId, Long waitingNumber) {
+        return Jwts.builder()
+                .claim("userId", userId)
+                .claim("waitingNumber", waitingNumber)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
+
