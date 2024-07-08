43e996ce62dcc0f7ea120ae7664d8bc19fa34994
jikimee64 <jikimee64@gmail.com>
2024-07-08 20:20:28 +0900

    feat: API 전역 예외처리 구현

---
+++ src/main/java/hhplus/concert/api/ApiControllerAdvice.java
@@ -0,0 +1,31 @@
+package hhplus.concert.api;
+
+import hhplus.concert.api.support.ApiResponse;
+import hhplus.concert.api.support.ApiException;
+import hhplus.concert.api.support.error.ErrorCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiResponse<?>> handleCoreApiException(ApiException e) {
+        return new ResponseEntity<>(ApiResponse.error(e.getErrorCode(), e.getData()), OK);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error("Exception : {}", e.getMessage(), e);
+        return new ResponseEntity<>(ApiResponse.error(ErrorCode.E500), INTERNAL_SERVER_ERROR);
+    }
+
+}
--- src/main/java/hhplus/concert/api/support/error/ErrorCode.java
+++ src/main/java/hhplus/concert/api/support/error/ErrorCode.java
@@ -6,7 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    E001("001", "An unexpected error has occurred.")
+    E001("001", "대기열 상태가 활성상태가 아닙니다."),
+    E002("002", "좌석이 이미 예약 되어 있습니다."),
+    E003("003", "충전 금액은 0이상 가능합니다."),
+    E004("004", "유효하지 않은 대기열 토큰입니다."),
+    E005("005", "결제 잔액이 부족합니다."),
+    E500("500", "알 수 없는 에러입니다. 관리자한테 문의해주세요.")
     ;
 
     private final String code;
