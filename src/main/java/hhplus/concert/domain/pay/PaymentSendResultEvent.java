3529331feebf96942607cd0aedf3dd614e3ad85d
wc kim <wckim@smilelab.co.kr>
2024-08-06 19:18:08 +0900

    feat: 결제 완료 후 결제 및 예약 정보 전송 이벤트 구현

---
+++ src/main/java/hhplus/concert/domain/pay/PaymentSendResultEvent.java
@@ -0,0 +1,33 @@
+package hhplus.concert.domain.pay;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class PaymentSendResultEvent {
+
+    private final Long userId;
+    private final String concertTitle;
+    private final LocalDate concertOpenDate;
+    private final LocalDateTime concertStartAt;
+    private final LocalDateTime concertEndAt;
+    private final Integer seatAmount;
+    private final Integer seatPosition;
+    private final LocalDateTime reservedAt;
+    private final LocalDateTime paymentedAt;
+
+    public PaymentSendResultEvent(Long userId, String concertTitle, LocalDate concertOpenDate, LocalDateTime concertStartAt,
+        LocalDateTime concertEndAt,
+        Integer seatAmount, Integer seatPosition, LocalDateTime reservedAt, LocalDateTime paymentedAt) {
+        this.userId = userId;
+        this.concertTitle = concertTitle;
+        this.concertOpenDate = concertOpenDate;
+        this.concertStartAt = concertStartAt;
+        this.concertEndAt = concertEndAt;
+        this.seatAmount = seatAmount;
+        this.seatPosition = seatPosition;
+        this.reservedAt = reservedAt;
+        this.paymentedAt = paymentedAt;
+    }
+}
